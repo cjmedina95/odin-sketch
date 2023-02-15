d55e7b470b951543b9d3e0be218b5fedd19d8125
Christian <Christian.Medina.ATX@gmail.com>
2023-02-14 22:30:12 -0600

    Update sizing to grid for more accurate drawing

--- js/main.js
+++ js/main.js
@@ -1,26 +1,75 @@
+// Generates a user-defined block of X*X cells.
+// cellSize has 0.01 subtracted due to odd numbers causing rounding errors,
+// leading to strange margin sizes and wrapping issues. 
 
-function addDiv(node) {
-    const addedDiv = document.createElement("div");
-    addedDiv.className = "cell";
-    node.appendChild(addedDiv, node);
+function generateGrid(gridSize, sketcherSize, sketcher) {
+    sketcher.textContent = "";
+    generateCells(gridSize, sketcherSize, sketcher);
+    sketcher.style.grid = "repeat(" + gridSize + ", " + (sketcherSize / gridSize) + "px) / auto-flow " + (sketcherSize / gridSize) + "px";
 }
 
-// Bitwise OR floors decimal points, then sets the first 4 bits to 1
-// e.g. 1267 -> 10011110011 |
-//        15 -> 00000001111
-//    = 1279 -> 10011111111
-// Then 1 is added to create the rounded number (1280).
-// ** Note: this formula only works for rounding on powers of 2.
+function generateCells(cellCount, size, sketchBoard) {
+    cellSize = size / cellCount;
 
-const sketcher = document.querySelector("#sketchContainer");
-const sketcherWidth = ((window.innerWidth * 0.66) | 15) + 1;
-const sketcherHeight = ((window.innerHeight * 0.66) | 15) + 1;
-
-sketcher.style.width = sketcherWidth.toString() + "px";
-sketcher.style.height = sketcherHeight.toString() + "px";
-
-for (i = 0; i < sketcherWidth; i += 16) {
-    for (j = 0; j < sketcherHeight; j += 16) {
-        addDiv(sketcher);
+    for (i = 0; i < (cellCount * cellCount); i++) {
+        addCell(sketchBoard, cellSize);
     }
 }
+
+function randomColor(currentColor) {
+    const red = Math.random() * 255;
+    const green = Math.random() * 255;
+    const blue = Math.random() * 255;
+
+    return "rgb(" + red + "," + green + ", " + blue + ")";
+}
+
+var mouseDown = false;
+
+function addCell(node, cellSize) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    cell.style.width = (cellSize-1) + "px";
+    cell.style.height = (cellSize-1) + "px";
+    cell.addEventListener("mousedown", (event) => {
+        event.target.style.backgroundColor = randomColor(event.target.style.backgroundColor);
+        mouseDown = true;
+    }, false);
+    cell.addEventListener("mouseup", (event) => {
+        mouseDown = false;
+    })
+    cell.addEventListener("mouseenter", (event) => {
+        if (mouseDown) {
+            event.target.style.backgroundColor = randomColor(event.target.style.backgroundColor);
+        }
+    }, false);
+    node.appendChild(cell, node);
+}
+
+const sketcher = document.querySelector("#sketchContainer");
+
+const sketcherSize = 512;
+
+sketcher.style.width = sketcherSize.toString() + "px";
+sketcher.style.height = sketcherSize.toString() + "px";
+
+const newButton = document.querySelector("#new");
+const resetButton = document.querySelector("#reset");
+
+let gridSize = 30;
+
+newButton.onclick = function() {
+    gridSize = prompt("Enter a new grid size between 1 and 100: ");
+
+    while (isNaN(gridSize) || gridSize < 1 || gridSize > 100 || gridSize % 1 != 0) {
+        gridSize = prompt("Invalid input. Enter a new grid size between 1 and 100:");
+    }
+    
+    generateGrid(gridSize, sketcherSize, sketcher);
+};
+
+resetButton.onclick = function() {
+    generateGrid(gridSize, sketcherSize, sketcher);
+};
+
+generateGrid(gridSize, sketcherSize, sketcher);
